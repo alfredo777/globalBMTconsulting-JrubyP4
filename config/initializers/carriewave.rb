bd48d7a024c3593fedb000481ce128f3293e167e
José Alfredo Reyes de Antuñano <jose_alfredo@cursa.me>
2017-05-09 14:35:32 -0500

    carriewave

--- config/initializers/carriewave.rb
+++ config/initializers/carriewave.rb
@@ -20,7 +20,7 @@ CarrierWave.configure do |config|
     }
    
     # For testing, upload files to local `tmp` folder.
-    if Rails.env.development? || Rails.env.cucumber?
+    if Rails.env.development?
       config.storage = :file
       config.enable_processing = false
       config.root = "#{Rails.root}/tmp"
